b8fb198256b4fcbd6d4ad7a13fd18c72451d5a24
peckn <70550012+peckn@users.noreply.github.com>
2020-10-12 15:39:25 -0400

    Add files via upload

---
+++ LAB-03_Template/Debug/startup/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../startup/startup_stm32f769xx.s 
+
+OBJS += \
+./startup/startup_stm32f769xx.o 
+
+S_DEPS += \
+./startup/startup_stm32f769xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+startup/startup_stm32f769xx.o: ../startup/startup_stm32f769xx.s
+	arm-none-eabi-gcc -mcpu=cortex-m7 -g3 -c -I"C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/LAB-03_Template/inc" -x assembler-with-cpp -MMD -MP -MF"startup/startup_stm32f769xx.d" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
---
+++ LAB-03_Template/Debug/stm32lib/CMSIS/Device/ST/STM32F7xx/Source/Templates/system_stm32f7xx.d
@@ -0,0 +1,83 @@
+stm32lib/CMSIS/Device/ST/STM32F7xx/Source/Templates/system_stm32f7xx.o: \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Device/ST/STM32F7xx/Source/Templates/system_stm32f7xx.c \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Device/ST/STM32F7xx/Include/stm32f769xx.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Include/core_cm7.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Include/cmsis_version.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Include/mpu_armv7.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/LAB-03_Template/inc/stm32f7xx_hal_conf.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_spi.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_spi_ex.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h \
+ C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart_ex.h
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Device/ST/STM32F7xx/Include/stm32f769xx.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Include/core_cm7.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Include/cmsis_version.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/LAB-03_Template/inc/stm32f7xx_hal_conf.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_spi.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_spi_ex.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h:
+
+C:/Users/peckn/STM32CubeIDE/workspace_1.4.0/stm32lib/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart_ex.h:
